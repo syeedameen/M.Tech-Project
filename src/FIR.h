50c7766b1ec6cebf9f623fec5c4c3621ee179ac0
Syeed Mohd Ameen <68741942+syeedameen@users.noreply.github.com>
2022-08-08 22:16:31 +0530

    Update FIR.h

--- src/FIR.h
+++ src/FIR.h
@@ -1,12 +1,8 @@
 /*
   LIBRARY CODE STARTS HERE
- 
- http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1279123369
- 
- Made into a library by AlphaBeta (www.AlexanderBrevig.com)
- 
- Do whatever you want with it.
- */
+*/
+
+
 #ifndef FIR_h
 #define FIR_h
 
@@ -25,7 +21,7 @@ public:
 
   void setCoefficients(float *newCoefs);
 
-  //set coefficient at specified index
+                                                  /* set coefficient at specified index */
   void setCoefficient(int idx, float newCoef);
 
   float process(float in);
@@ -35,10 +31,10 @@ private:
 
   float coef[FILTERTAPS];
 
-  //declare gain coefficient to scale the output back to normal
-  float gain; // set to 1 and input unity to see what this needs to be
+                                /* declare gain coefficient to scale the output back to normal */
+  float gain;                   /* set to 1 and input unity to see what this needs to be */
 
-  int k; // k stores the index of the current array read to create a circular memory through the array
+  int k;                        /* k stores the index of the current array read to create a circular memory through the array */
 };
 #endif
 
